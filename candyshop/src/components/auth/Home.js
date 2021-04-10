e1f62c54e543720757581c99aefceb28362cda7c
Dawar Waqar <77901465+DawarWaqar@users.noreply.github.com>
2021-04-11 01:57:18 +0500

    Update1

--- candyshop/src/components/auth/Home.js
+++ candyshop/src/components/auth/Home.js
@@ -11,104 +11,35 @@ import {
 import api from "./api"
 
 
-
 class Home extends Component {
-    state = {
-        email: "",
-        password: "",
-        errors: {}
-    }
+   
 
-    onChange = e => {
-        this.setState({ [e.target.id]: e.target.value });
-    }
-    onSubmit = e => {
-        e.preventDefault();
-        const userData = {
-            email: this.state.email,
-            password: this.state.password
-        }
-
-        api("auth/customer/login", userData, 200).then(res => {
-            
-            if (res.statusCode == 200) {
-                window.localStorage.setItem('token', res.token);
-                this.props.history.push("/home/customer");
-                
-            }
-            else {
-                alert("Error")
-            }
-        }
-        ).catch(e => console.log(e))
-    }
 
     render() {
 
         return (
-            // <div className="home-page">
+
             
           <container>
-            
-                <div className="container main">
-                {/* <div className="btn-handler">         
-                    <Button className="menuu-btn" onClick={() => {
-                       
-                    }}>MENU</Button>
-                </div>
-
-                <div className="aboutt-btn-handler">         
-                    <Button className="aboutt-btn" onClick={() => {
-                        
-                    }}>ABOUT US</Button>
-                </div>
-
-                <div className="btn-handler">         
-                    <Button className="loginn-btn" onClick={() => {
-                        this.props.history.push('/login');
-                    }}>LOGIN</Button>
-                </div> */}
-                
-                    {/*<p className="brand-name">CANDY SHOP</p>*/}
-                    {/* <p className="title">Login</p> */}
-                    <Form className="reg-form" noValidate onSubmit={this.onSubmit}>
-                        {/* <FormGroup>
-                            <Input
-                                type="email"
-                                placeholder="Enter your email address"
-                                onChange={this.onChange}
-                                value={this.state.email}
-                                id="email"
-                            />
-                        </FormGroup> */}
-                        {/* <FormGroup className="password-container">
-                            <Input
-                                type="password"
-                                placeholder="Enter your password"
-                                onChange={this.onChange}
-                                value={this.state.password}
-                                id="password"
-                            />
-                        </FormGroup> */}
-                        {/* <div className="btn-handler">
-                            <Link to="/forgot-password/customer" className="link" style={{ marginleft: "14%" }}>Forgot Password? :(</Link>
-                        </div> */}
-                        <div className="order-btn-handler">
-                            
-                                
-                            <Button className="orderNow-btn" onClick={() => {
-                                this.props.history.push('/menu');
+            <figure className="position-relative">
+            <img src={bg} alt="bg" className="img-fluid"></img>
+            <figcaption>
+            <Button className="orderNow-btn" onClick={() => {
+                                this.props.history.push('/login');
                             }}>Order Now</Button>
-                        </div>
+            </figcaption>
+            </figure>
+               
+                
+                            
+                            
+                       
 
                         
 
 
-                        {/* <div className="btn-handler">
-                            <Link to="/login/admin" className="link">Login as Admin</Link>
-                        </div> */}
-                    </Form>
-                </div>
+                    
+                
          
          </container>
         
