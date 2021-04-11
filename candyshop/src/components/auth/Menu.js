fdb4e57d07556f3c23bb0e133e1908324b333cd7
Dawar Waqar <77901465+DawarWaqar@users.noreply.github.com>
2021-04-11 15:50:50 +0500

    Update4pm

--- candyshop/src/components/auth/Menu.js
+++ candyshop/src/components/auth/Menu.js
@@ -5,8 +5,31 @@ import m from  "../img/medium.png";
 import l from  "../img/large.png";
 import bg from "../img/bgMenu.png";
 import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
+import api from "./api"
 class Menu extends Component {
-    
+    state = 0
+
+    onSubmit = e => {
+        e.preventDefault();
+        const userData = {
+            boxW: this.state
+        }
+        console.log("ud ",userData)
+
+        api("auth/customer/login", userData, 200).then(res => {
+            
+            if (res.statusCode == 200) {
+                window.localStorage.setItem('token', res.token);
+                this.props.history.push("/home/customer");
+                
+            }
+            else {
+                alert("Error")
+            }
+        }
+        ).catch(e => console.log(e))
+    }
     render(){
         let text = "CLICK ON THE PREFERRED BOX :)"
         return(
@@ -16,7 +39,7 @@ class Menu extends Component {
                     <div id="menu-outer"  >
                         <div className="tableD" >
                             <ul id="horizontal-list" className="manualBg" >
-                                <li className="space" value="160"><a href='http://localhost:3000/menu2'><img src={s} /></a></li>
+                                <li className="space" onClick={()=>this.setState(160)}><a href='http://localhost:3000/menu2'><img src={s} /></a></li>
                                 <li className="space"><a href='http://localhost:3000/menu2'><img src={m} /></a></li>
                                 <li className="space"><a href='http://localhost:3000/menu2'><img src={l} /></a></li>
                             </ul>
---
+++ candyshop/src/components/auth/Menu2.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from "react";
+import s from  "../img/small.png";
+import m from  "../img/medium.png";
+import l from  "../img/large.png";
+import bg from "../img/bgMenu.png";
+import { Container } from "reactstrap";
+import { Link } from "react-router-dom";
+import api from "./api"
+class Menu2 extends Component {
+    
+    render(){
+        
+        return(
+               
+                <div>hiii</div>
+        
+    
+        )
+    }
+    }
+
+    export default Menu2;   
