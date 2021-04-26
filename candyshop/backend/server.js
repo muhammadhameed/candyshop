5af9fe8d8d955cb91d669604b34ec77637607af1
ayantaby1 <ayanster103@gmail.com>
2021-04-26 14:37:17 +0500

    df

--- candyshop/backend/server.js
+++ candyshop/backend/server.js
@@ -50,49 +50,12 @@ if (process.env.NODE_ENV === "production")
     });
 }
 
-// if (process.env.NODE_ENV === "production")
-// {
-//     console.log("reached")
-//     var distDir = __dirname + "/dist/";
-//     app.use(express.static(path.join(__dirname, '../src/build/index.html')));
-//     console.log("blah")
 
-//     app.get('/', function(req, res) {
-//         //res.sendFile(path.resolve(__dirname,  "build", "Home.js"))
-//         res.sendFile(path.join(__dirname, '../src', 'build', 'index.html'));
-//         console.log("blah")
-//     });
-// }
+app.get('/home', function(req, res) {
+    res.sendFile(path.join(__dirname + '../src/components/auth/Home.js'));
+});
 
 
-const CustomersRouter = require('./routes/customers');
-const AdminRouter = require('./routes/admin');
-const ShoppingCartRouter = require('./routes/shoppingCart');
-const ProductRouter = require('./routes/product');
-const DiscountRouter = require('./routes/discount');
-const OrdersRouter = require('./routes/orders');
-
-app.use('/customers', CustomersRouter);
-app.use('/admin', AdminRouter);
-app.use('/shoppingCart', ShoppingCartRouter);
-app.use('/product', ProductRouter);
-app.use('/discount', DiscountRouter);
-app.use('/orders', OrdersRouter);
-
-
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static ('candyshop/build'));
-
-//     app.get('*', (req,res) => {
-//         res.sendFile(path.join(_dirname, 'candyshop', 'build', 'index.html'));
-
-//     }); 
-// }
-
-// app.use(express.static(path.join(_dirname, '../build')));
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build'))
-// })
 
 
 app.listen(port, () => {
